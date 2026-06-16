5e238cef06bebde7c9d784d0ef654307e11362d3
Ubuntu <ubuntu@ip-172-31-34-70.eu-north-1.compute.internal>
2026-06-16 17:04:02 +0000

    microservices nodejs project api gateway

--- api-gateway/URLs.js
+++ api-gateway/URLs.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ORDERS_API_URL: 'http://localhost:3005',
-    PRODUCTS_API_URL: 'http://localhost:3006',
+    ORDERS_API_URL: 'http://orders_cont:3000',
+    PRODUCTS_API_URL: 'http://products_cont:3000',
   };
